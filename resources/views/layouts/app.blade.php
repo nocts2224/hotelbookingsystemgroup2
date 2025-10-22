2ce58c21b0c0d0e2742709d9e693846ea5e7038a
nocts2224 <ljgabrielle22242@gmail.com>
2025-10-23 00:59:55 +0800

    icon changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <title>Hotel Booking System</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" />
-
+    <link rel="icon" href="../images/five-stars.png">
 </head>
 <body>
     @include('partials.navbar')
